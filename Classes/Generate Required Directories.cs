57f4308eee19618008a2414bb5581a790edb536d
Jackson-ABC <jackson@abcsheds.net.au>
2025-07-31 16:49:49 +1000

    refactor(file-structure): simplify directory creation logic

--- Classes/Generate Required Directories.cs
+++ Classes/Generate Required Directories.cs
@@ -8,30 +8,28 @@ public class GenerateRequiredDirectories
     /// <param name="inputFilePath">The file path of the workbook</param>
     /// <param name="fileType">The file type of the workbook (e.g. .xlsx, .xltx, .xlsm, .xltm, .xlam)</param>
     /// <param name="outputDir">The directory to create the workbook directory in</param>
-    public static string Run(string inputFilePath, string fileType, string outputDir)
+    public static string Run(string inputFilePath, string fileType,  string savePath)
     {
         // Create "Workbook" directory
-        string workbookName = Path.GetFileNameWithoutExtension(inputFilePath) + "_internals";
-        string workbookDir = Path.Combine(outputDir, workbookName);
-        Directory.CreateDirectory(workbookDir);
+        Directory.CreateDirectory(savePath);
 
         // Create "Sheets" directory
-        Directory.CreateDirectory(Path.Combine(workbookDir, "Sheets"));
+        Directory.CreateDirectory(Path.Combine(savePath, "Sheets"));
 
-        if(fileType == ".xl?m")
+        if (fileType == ".xl?m")
         {
             // Create "VBA" directories
-            Directory.CreateDirectory(Path.Combine(workbookDir, "VBA"));
-            Directory.CreateDirectory(Path.Combine(workbookDir, "VBA", "Microsoft Excel Objects"));
-            Directory.CreateDirectory(Path.Combine(workbookDir, "VBA", "Modules"));
-            Directory.CreateDirectory(Path.Combine(workbookDir, "VBA", "Classes"));
-            Directory.CreateDirectory(Path.Combine(workbookDir, "VBA", "Forms"));
+            Directory.CreateDirectory(Path.Combine(savePath, "VBA"));
+            Directory.CreateDirectory(Path.Combine(savePath, "VBA", "Microsoft Excel Objects"));
+            Directory.CreateDirectory(Path.Combine(savePath, "VBA", "Modules"));
+            Directory.CreateDirectory(Path.Combine(savePath, "VBA", "Classes"));
+            Directory.CreateDirectory(Path.Combine(savePath, "VBA", "Forms"));
 
             // Create "RibbonX" directory (Custom Ribbons)
-            Directory.CreateDirectory(Path.Combine(workbookDir, "RibbonX"));
-            Directory.CreateDirectory(Path.Combine(workbookDir, "RibbonX", "Icons"));
+            Directory.CreateDirectory(Path.Combine(savePath, "RibbonX"));
+            Directory.CreateDirectory(Path.Combine(savePath, "RibbonX", "Icons"));
         }
 
-        return workbookDir;
+        return savePath;
     }
 }
