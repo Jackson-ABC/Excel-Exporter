5ff8b1750e0edb90832c08250cdcc1516c045554
Jackson-ABC <jackson@abcsheds.net.au>
2025-07-31 11:47:24 +1000

    feat(resources): add allowed file types set

---
+++ Resources/GlobalDictionaries.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelExporter.Resources
+{
+    static class GlobalDictionaries
+    {
+        public static HashSet<string> AllowedFileTypes = new HashSet<string>
+        {
+            "xlsx",
+            "xltx",
+            "xlsm",
+            "xltm",
+            "xlam"
+        };
+    }
+}
