8ee5ff75abacd4f7d8ef2bbfda33b43de8dea5ad
Jackson-ABC <jackson@abcsheds.net.au>
2025-09-03 09:33:30 +1000

    WIP: feat(json-handling): create json export class

---
+++ Classes/JSON Handler.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace ExcelExporter.Classes
+{
+    internal class JSON_Handler
+    {
+        public static void Write(object[,] array, string outputDir, string filename)
+        {
+            string jsonPath = Path.Combine(outputDir, filename + ".json");
+
+            var sb = new StringBuilder();
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+        }
+    }
+}
